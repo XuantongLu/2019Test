4a6e10e1790b6192d1413a9aac8346f60fb81bb8
luxua <luxua@LAPTOP-5NTV2VDT>
2019-04-08 14:16:23 +0200

    new

--- src/testgithub/TestGithub.java
+++ src/testgithub/TestGithub.java
@@ -16,7 +16,7 @@ public class TestGithub {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("abcbbb");
+        System.out.println("abc");
     }
     
 }
